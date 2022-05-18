7a0593d5f48b8ea181e43074855e3a8d62ee0c6e
Kraghus <masongage@outlook.com>
2022-05-18 11:19:40 -0400

    Moved functions outside of main
    Created io file to hold program functions.

---
+++ io.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+double getDouble()
+{
+	std::cout << "Enter a double value: ";
+	double number{};
+	std::cin >> number;
+	return number;
+}
+
+char getOperator()
+{
+	std::cout << "Enter one of the following operators: +, -, *, or /: ";
+	char op{};
+	std::cin >> op;
+	return op;
+}
+
+void printCalculation(double firstDouble, double secondDouble, char op)
+{
+	if (op == '+')
+		std::cout << firstDouble << " + " << secondDouble << " is " << (firstDouble + secondDouble) << '\n';
+	else if (op == '-')
+		std::cout << firstDouble << " - " << secondDouble << " is " << (firstDouble - secondDouble) << '\n';
+	else if (op == '*')
+		std::cout << firstDouble << " * " << secondDouble << " is " << (firstDouble * secondDouble) << '\n';
+	else if (op == '/')
+		std::cout << firstDouble << " / " << secondDouble << " is " << (firstDouble / secondDouble) << '\n';
+	else
+		std::cout << "You entered an invalid operator. Try again.\n";
+}
--- main.cpp
+++ main.cpp
@@ -1,34 +1,9 @@
 #include <iostream>
 
-double getDouble()
-{
-	std::cout << "Enter a double value: ";
-	double number{};
-	std::cin >> number;
-	return number;
-}
+double getDouble();
+char getOperator();
+void printCalculation(double firstDouble, double secondDouble, char op);
 
-char getOperator()
-{
-	std::cout << "Enter one of the following operators: +, -, *, or /: ";
-	char op{};
-	std::cin >> op;
-	return op;
-}
-
-void printCalculation(double firstDouble, double secondDouble, char op)
-{
-	if (op == '+')
-		std::cout << firstDouble << " + " << secondDouble << " is " << (firstDouble + secondDouble);
-	else if (op == '-')
-		std::cout << firstDouble << " - " << secondDouble << " is " << (firstDouble - secondDouble);
-	else if (op == '*')
-		std::cout << firstDouble << " * " << secondDouble << " is " << (firstDouble * secondDouble);
-	else if (op == '/')
-		std::cout << firstDouble << " / " << secondDouble << " is " << (firstDouble / secondDouble);
-	else
-		std::cout << "You entered an invalid operator. Try again.";
-}
 int main()
 {
 	double firstDouble{ getDouble() };					//asks user for the first double value
