eb0260ae6778e6bf715e3465dd7f4f985ff25ae3
Kraghus <masongage@outlook.com>
2022-05-19 08:22:04 -0400

    Changed function call order
    Changed the function call order to make the program perform more like a traditional calculator.

--- main.cpp
+++ main.cpp
@@ -7,12 +7,12 @@ int main()
 {
 	/* asks user for the first double value */
 	double firstDouble{ getDouble() };	
-
-	/* asks user for the second double value */
-	double secondDouble{ getDouble() };	
 	
 	/* asks user which operator to use */
 	char op{ getOperator() };			
+
+	/* asks user for the second double value */
+	double secondDouble{ getDouble() };
 
 	/* passes input to be calculated and printed */
 	printCalculation(firstDouble, secondDouble, op);
