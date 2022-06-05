9300810c76c3c429e427ce024493a22f3c448cd2
Kraghus <masongage@outlook.com>
2022-06-05 11:23:58 -0400

    Update main.cpp
    Added a loop to try again if the user enters invalid input at the end of the program. Previously, it just terminated the program.

--- main.cpp
+++ main.cpp
@@ -20,10 +20,17 @@ int main()
 		// uses stored values to pass user input in order to be calculated and printed
 		printCalculation(firstDouble, secondDouble, op);
 
+	// return here if the user entered an invalid character
+	// not best practice, will create a looping function in the future
+	tryAgain:
 		std::cout << "Enter 'X' to exit or 'C' to continue.\n";
 		char exitOrContinue{};
 		std::cin >> exitOrContinue;
 
+		// clears any extraneous input
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+		// asks the user to exit or continue the program loop
 		if ((exitOrContinue == 'X') || (exitOrContinue == 'x'))
 		{
 			return 0;
@@ -34,8 +41,8 @@ int main()
 		}
 		else
 		{
-			std::cout << "Invalid input detected... terminating program.\n";
-			return 1;
+			std::cout << "Invalid input detected... try again.\n";
+			goto tryAgain;
 		}
 
 	}
