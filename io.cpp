9a2e49483dba4df73df729ab202ec19e6ed289fd
Kraghus <masongage@outlook.com>
2022-06-06 11:06:40 -0400

    Formatting and flow update
    Changed some formatting and  changed a goto statement to a while loop.

--- io.cpp
+++ io.cpp
@@ -58,25 +58,25 @@ void printCalculation(double firstDouble, double secondDouble, char op)
 	{
 	case '+':
 		std::cout << firstDouble << " + " << secondDouble << " is " << (firstDouble + secondDouble) << '\n';
-		std::cout << "=========================================================\n";
+		std::cout << "=========================================================\n\n";
 		break;
 	case '-':
 		std::cout << firstDouble << " - " << secondDouble << " is " << (firstDouble - secondDouble) << '\n';
-		std::cout << "=========================================================\n";
+		std::cout << "=========================================================\n\n";
 		break;
 	case '*':
 		std::cout << firstDouble << " * " << secondDouble << " is " << (firstDouble * secondDouble) << '\n';
-		std::cout << "=========================================================\n";
+		std::cout << "=========================================================\n\n";
 		break;
 	case '/':
 		std::cout << firstDouble << " / " << secondDouble << " is " << (firstDouble / secondDouble) << '\n';
-		std::cout << "=========================================================\n";
+		std::cout << "=========================================================\n\n";
 		break;
 	case '%':
 		std::cout << firstDouble << " % " << secondDouble << " is " <<
 				  // Converts double values to integer values in order to use modulus operator.
 			      (static_cast<int>(firstDouble) % static_cast<int>(secondDouble)) << '\n';
-		std::cout << "=========================================================\n";
+		std::cout << "=========================================================\n\n";
 		break;
 	default:
 		std::cout << "You entered an invalid operator. Try again.\n";
--- main.cpp
+++ main.cpp
@@ -8,6 +8,9 @@ int main()
 {
 	while (true)
 	{
+		// formatting
+		std::cout << "=========================================================\n";
+
 		// asks the user for the first double value and stores the return value as type int
 		double firstDouble{ getDouble() };
 
@@ -20,30 +23,31 @@ int main()
 		// uses stored values to pass user input in order to be calculated and printed
 		printCalculation(firstDouble, secondDouble, op);
 
-	// return here if the user entered an invalid character
-	// not best practice, will create a looping function in the future
-	tryAgain:
-		std::cout << "Enter 'X' to exit or 'C' to continue.\n";
-		char exitOrContinue{};
-		std::cin >> exitOrContinue;
-
-		// clears any extraneous input
-		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-
-		// asks the user to exit or continue the program loop
-		if ((exitOrContinue == 'X') || (exitOrContinue == 'x'))
+		// loops until the user decides to continue or exit
+		while (true)
 		{
-			return 0;
-		}
-		else if ((exitOrContinue == 'C') || (exitOrContinue == 'c'))
-		{
-			continue;
-		}
-		else
-		{
-			std::cout << "Invalid input detected... try again.\n";
-			goto tryAgain;
-		}
+			std::cout << "Enter 'X' to exit or 'C' to continue.\n";
+			char exitOrContinue{};
+			std::cin >> exitOrContinue;
 
+			// clears any extraneous input
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+			// asks the user to exit or continue the program loop
+			if ((exitOrContinue == 'X') || (exitOrContinue == 'x'))
+			{
+				return 0;
+			}
+			else if ((exitOrContinue == 'C') || (exitOrContinue == 'c'))
+			{
+				std::cout << "\n";
+				break;
+			}
+			else
+			{
+				std::cout << "Invalid input detected... try again.\n\n";
+				continue;
+			}
+		}
 	}
 }
